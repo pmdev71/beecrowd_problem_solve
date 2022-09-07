7fb9ba2197f4e2a66df892440ecb8306b4750e92
Palash Mandal <palash15-2092@diu.edu.bd>
2022-09-07 18:06:24 +0600

    1008	Salary

--- solve.c
+++ solve.c
@@ -2,10 +2,12 @@
 int main ()
 
 {
-    int A,B,C,D,sub;
-    scanf("%d%d%d%d",&A,&B,&C,&D);
-    sub=((A*B)-(C*D));
-    printf("DIFERENCA = %d\n",sub);
+    int e_number,w_hour;
+    float a_hour,salary;
+    scanf("%d%d%f",&e_number,&w_hour,&a_hour);
+    salary=w_hour*a_hour;
+    printf("NUMBER = %d\n",e_number);
+    printf("SALARY = U$ %.2f\n",salary);
     return 0;
 
 }
