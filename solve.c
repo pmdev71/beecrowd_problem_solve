d6bc4a8652dbc266cbe979e00093cfbe0b93148a
Palash Mandal <palash15-2092@diu.edu.bd>
2022-09-10 16:16:47 +0600

    1010	Simple Calculate

--- solve.c
+++ solve.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
-int main ()
-
+# include<stdio.h>
+int main()
 {
-    char name;
-    double s,e,sum;
-    scanf("%s %lf %lf", &name, &s, &e);
-    sum= s+e*.15;
-    printf("TOTAL = R$ %.2lf\n", sum);
+    int x1,y1,x2,y2;
+    float z1,z2,result;
+    scanf("%d %d %f",&x1,&y1,&z1);
+    scanf("%d %d %f",&x2,&y2,&z2);
+    result=(z1*y1)+(z2*y2);
+    printf("VALOR A PAGAR: R$ %.2f\n",result);
     return 0;
 }
