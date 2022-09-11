94c6d3d3b71cb3b25f01c461acb27a2942ac85d8
Palash Mandal <palash15-2092@diu.edu.bd>
2022-09-11 22:23:24 +0600

    1011	Sphere

--- solve.c
+++ solve.c
@@ -1,11 +1,9 @@
 # include<stdio.h>
 int main()
 {
-    int x1,y1,x2,y2;
-    float z1,z2,result;
-    scanf("%d %d %f",&x1,&y1,&z1);
-    scanf("%d %d %f",&x2,&y2,&z2);
-    result=(z1*y1)+(z2*y2);
-    printf("VALOR A PAGAR: R$ %.2f\n",result);
+    double x,result;
+    scanf("%lf",&x);
+    result=(4*3.14159*(x*x*x))/3;
+    printf("VOLUME = %.3lf\n",result);
     return 0;
 }
