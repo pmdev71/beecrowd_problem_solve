d5f44679bc933ca0ad6a1357e548278d915ff974
Palash Mandal <palash15-2092@diu.edu.bd>
2022-09-12 21:59:09 +0600

    1012	Area

--- solve.c
+++ solve.c
@@ -1,9 +1,18 @@
-# include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    double x,result;
-    scanf("%lf",&x);
-    result=(4*3.14159*(x*x*x))/3;
-    printf("VOLUME = %.3lf\n",result);
+    double A,B,C,R1,R2,R3,R4,R5;
+    scanf("%lf%lf%lf",&A,&B,&C);
+    R1=(A*C)/2;
+    R2=3.14159*(C*C);
+    R3=.5*(A+B)*C;
+    R4=B*B;
+    R5=A*B;
+    printf("TRIANGULO: %.3lf\n",R1);
+    printf("CIRCULO: %.3lf\n",R2);
+    printf("TRAPEZIO: %.3lf\n",R3);
+    printf("QUADRADO: %.3lf\n",R4);
+    printf("RETANGULO: %.3lf\n",R5);
     return 0;
 }
