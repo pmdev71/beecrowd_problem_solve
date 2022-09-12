0664313b6416f528c099348727b73cacd1c3dc47
Palash Mandal <palash15-2092@diu.edu.bd>
2022-09-12 23:39:04 +0600

    1013	The Greatest

--- solve.c
+++ solve.c
@@ -2,17 +2,12 @@
 
 int main()
 {
-    double A,B,C,R1,R2,R3,R4,R5;
-    scanf("%lf%lf%lf",&A,&B,&C);
-    R1=(A*C)/2;
-    R2=3.14159*(C*C);
-    R3=.5*(A+B)*C;
-    R4=B*B;
-    R5=A*B;
-    printf("TRIANGULO: %.3lf\n",R1);
-    printf("CIRCULO: %.3lf\n",R2);
-    printf("TRAPEZIO: %.3lf\n",R3);
-    printf("QUADRADO: %.3lf\n",R4);
-    printf("RETANGULO: %.3lf\n",R5);
+   int A,B,C,maior,max;
+    scanf("%d%d%d",&A,&B,&C);
+    maior=((A+B+ abs(A-B))/2);
+    max=((maior+C+ abs(maior-C))/2);
+    
+    printf("%d eh o maior\n",max);
+    
     return 0;
 }
