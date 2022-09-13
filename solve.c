d2127011622199affb097ee04a45c2575205d752
Palash Mandal <palash15-2092@diu.edu.bd>
2022-09-13 23:20:43 +0600

    1014	Consumption

--- solve.c
+++ solve.c
@@ -2,12 +2,10 @@
 
 int main()
 {
-   int A,B,C,maior,max;
-    scanf("%d%d%d",&A,&B,&C);
-    maior=((A+B+ abs(A-B))/2);
-    max=((maior+C+ abs(maior-C))/2);
-    
-    printf("%d eh o maior\n",max);
-    
+    int X;
+    float Y,resulr;
+    scanf("%d%f",&X,&Y);
+    resulr= X/Y;
+    printf("%.3f km/l\n",resulr);
     return 0;
 }
